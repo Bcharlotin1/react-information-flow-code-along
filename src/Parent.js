2a3e8bf31ce88c908802c1827446874d4928eff3
bcharlotin1 <biancacharlotin1@gmail.com>
2021-11-24 16:39:09 -0500

    Done.

--- src/Parent.js
+++ src/Parent.js
@@ -7,15 +7,24 @@ class Parent extends Component {
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: "#FFF", //starts of white  in the rectangel
     }
   }
-
+  
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor, // this keeps track of the colore and changes the color of the  children to a random color
+    });
+  };
+//now both parent and child colroe are getting changed 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor} />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor} />
+    
       </div>
     )
   }
